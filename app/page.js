171879a71e962626b5522fa7a4f014858f4468c2
nhuzjak20 <BorusiaDortmund15>
2024-01-25 17:41:36 +0100

    Popravljeni Linkovi i Dodana Kartica

--- app/page.js
+++ app/page.js
@@ -1,60 +1,80 @@
-import Link from 'next/link'
+import Link from "next/link";
+import "./globals.scss";
 
 export default function Home() {
   return (
     <>
-      <div class="stars">
-  <div class="star"></div>
-  <div class="star"></div>
-  <div class="star"></div>
-  <div class="star"></div>
-  <div class="star"></div>
-  <div class="star"></div>
-  <div class="star"></div>
-  <div class="star"></div>
-  <div class="star"></div>
-  <div class="star"></div>
-  <div class="star"></div>
-  <div class="star"></div>
-  <div class="star"></div>
-  <div class="star"></div>
-  <div class="star"></div>
-  <div class="star"></div>
-  <div class="star"></div>
-  <div class="star"></div>
-  <div class="star"></div>
-  <div class="star"></div>
-  <div class="star"></div>
-  <div class="star"></div>
-  <div class="star"></div>
-  <div class="star"></div>
-  <div class="star"></div>
-  <div class="star"></div>
-  <div class="star"></div>
-  <div class="star"></div>
-  <div class="star"></div>
-  <div class="star"></div>
-  <div class="star"></div>
-  <div class="star"></div>
-  <div class="star"></div>
-  <div class="star"></div>
-  <div class="star"></div>
-  <div class="star"></div>
-  <div class="star"></div>
-  <div class="star"></div>
-  <div class="star"></div>
-  <div class="star"></div>
-  <div class="star"></div>
-  <div class="star"></div>
-  <div class="star"></div>
-  <div class="star"></div>
-  <div class="star"></div>
-  <div class="star"></div>
-  <div class="star"></div>
-  <div class="star"></div>
-  <div class="star"></div>
-  <div class="star"></div>
-</div>
+      <div className="stars">
+        <div className="star"></div>
+        <div className="star"></div>
+        <div className="star"></div>
+        <div className="star"></div>
+        <div className="star"></div>
+        <div className="star"></div>
+        <div className="star"></div>
+        <div className="star"></div>
+        <div className="star"></div>
+        <div className="star"></div>
+        <div className="star"></div>
+        <div className="star"></div>
+        <div className="star"></div>
+        <div className="star"></div>
+        <div className="star"></div>
+        <div className="star"></div>
+        <div className="star"></div>
+        <div className="star"></div>
+        <div className="star"></div>
+        <div className="star"></div>
+        <div className="star"></div>
+        <div className="star"></div>
+        <div className="star"></div>
+        <div className="star"></div>
+        <div className="star"></div>
+        <div className="star"></div>
+        <div className="star"></div>
+        <div className="star"></div>
+        <div className="star"></div>
+        <div className="star"></div>
+        <div className="star"></div>
+        <div className="star"></div>
+        <div className="star"></div>
+        <div className="star"></div>
+        <div className="star"></div>
+        <div className="star"></div>
+        <div className="star"></div>
+        <div className="star"></div>
+        <div className="star"></div>
+        <div className="star"></div>
+        <div className="star"></div>
+        <div className="star"></div>
+        <div className="star"></div>
+        <div className="star"></div>
+        <div className="star"></div>
+        <div className="star"></div>
+        <div className="star"></div>
+        <div className="star"></div>
+        <div className="star"></div>
+        <div className="star"></div>
+      </div>
+      <div className="grid grid-cols-3 grid-rows-3 gap-3">
+        <div className="grid row-span-1">
+          <div class="container grid-span-2 ">
+            <Link href="/v3produkt" legacyBehavior>
+              <a class="card1 glassy-background">
+                <h3>This is option 1</h3>
+                <p class="small">
+                  Card description with lots of great facts and interesting
+                  details.
+                </p>
+                <div class="go-corner" href="#">
+                  <div class="go-arrow">→</div>
+                </div>
+              </a>
+            </Link>
+          </div>
+        </div>
+        <div className="grid row-span-3">c</div>
+      </div>
     </>
   );
 }
---
+++ app/pages/v3produkt/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import GeneralKalkulator from "./GeneralKalkulator";
+
+function index() {
+  return (
+    <div>
+      <GeneralKalkulator></GeneralKalkulator>
+    </div>
+  );
+}
+
+export default index;
