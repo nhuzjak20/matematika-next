0eb9ecc2fe6193ec777667c67fcdeffdf85f8c0f
nhuzjak20 <BorusiaDortmund15>
2024-02-01 22:14:50 +0100

    unesena harmonika za postupak

--- app/ispit/gradijentfunkcije/Postupak.jsx
+++ app/ispit/gradijentfunkcije/Postupak.jsx
@@ -1,8 +1,50 @@
 import React from "react";
+import Accordion from "@mui/material/Accordion";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import Typography from "@mui/material/Typography";
+import math from "mathjs";
 
-function Postupak({}) {
+function pretvoriPIuBroj(str) {
+  const piSymbol = "π";
+  const piValue = Math.PI;
+
+  // Zamijeni sve pojave simbola π u stringu s vrijednošću broja Math.PI
+  const noviString = str.replace(new RegExp(piSymbol, "g"), piValue);
+  console.log("Novi string je " + noviString);
+  // Vrati novi string koji sadrži brojevnu vrijednost za simbol PI
+  return noviString;
+}
+
+function Postupak({ tx, ty, eq }) {
+  let xDerivacija = undefined;
+  let yDerivacija = undefined;
+  let TockaRijesenje1 = undefined;
+  let TockaRijesenje2 = undefined;
   console.log("Dobar dan");
-  return <div>postupak</div>;
+  try {
+    xDerivacija = math.derivative(jednadba, "x").toString();
+    yDerivacija = math.derivative(jednadba, "y").toString();
+    const tockaPretvoreno = [
+      pretvoriPIuBroj(tocka[0]),
+      pretvoriPIuBroj(tocka[1]),
+    ];
+    TockaRijesenje1 = math.evaluate(xDerivacija, { x: tockaPretvoreno[0] });
+    TockaRijesenje2 = math.evaluate(yDerivacija, { y: tockaPretvoreno[1] });
+    console.log(tockaPretvoreno);
+    console.log(TockaRijesenje1);
+  } catch (e) {
+    console.log("Error je izaso");
+    console.log(e);
+  }
+  return (
+    <>
+      <Accordion>
+        <AccordionSummary>Riješenje</AccordionSummary>
+        <AccordionDetails></AccordionDetails>
+      </Accordion>
+    </>
+  );
 }
 
 export default Postupak;
--- app/ispit/gradijentfunkcije/page.jsx
+++ app/ispit/gradijentfunkcije/page.jsx
@@ -1,39 +1,143 @@
+"use client";
 import React from "react";
+import Accordion from "@mui/material/Accordion";
+import { useState } from "react";
 import Postupak from "./postupak";
-import { Card } from "@mui/material";
-import { TextField } from "@mui/material";
-import { Typography } from "@mui/material";
+import Link from "next/link";
+import {
+  Card,
+  TextField,
+  Typography,
+  ButtonGroup,
+  Button,
+} from "@mui/material";
+
 import "../../globals.scss";
 
-function page() {
+function Page() {
+  const [eq, ChangeEq] = useState("");
+  const [tX, ChangeTX] = useState(0);
+  const [tY, ChangeTY] = useState(0);
+  const [rijeseno, Prepravi] = useState(0);
   return (
     <>
-      <Card className="flex flex-col items-center glassmorph">
+      <div className="stars">
+        <div className="star"></div>
+        <div className="star"></div>
+        <div className="star"></div>
+        <div className="star"></div>
+        <div className="star"></div>
+        <div className="star"></div>
+        <div className="star"></div>
+        <div className="star"></div>
+        <div className="star"></div>
+        <div className="star"></div>
+        <div className="star"></div>
+        <div className="star"></div>
+        <div className="star"></div>
+        <div className="star"></div>
+        <div className="star"></div>
+        <div className="star"></div>
+        <div className="star"></div>
+        <div className="star"></div>
+        <div className="star"></div>
+        <div className="star"></div>
+        <div className="star"></div>
+        <div className="star"></div>
+        <div className="star"></div>
+        <div className="star"></div>
+        <div className="star"></div>
+        <div className="star"></div>
+        <div className="star"></div>
+        <div className="star"></div>
+        <div className="star"></div>
+        <div className="star"></div>
+        <div className="star"></div>
+        <div className="star"></div>
+        <div className="star"></div>
+        <div className="star"></div>
+        <div className="star"></div>
+        <div className="star"></div>
+        <div className="star"></div>
+        <div className="star"></div>
+        <div className="star"></div>
+        <div className="star"></div>
+        <div className="star"></div>
+        <div className="star"></div>
+        <div className="star"></div>
+        <div className="star"></div>
+        <div className="star"></div>
+        <div className="star"></div>
+        <div className="star"></div>
+        <div className="star"></div>
+        <div className="star"></div>
+        <div className="star"></div>
+      </div>
+      <Card className="flex flex-col items-center glassy-background-white w-fit p-4 m-auto my-24">
         <div className="flex justify-center items-center p-2">
-          <Typography variant="h5">f(x,y)=</Typography>
-          <TextField variant="outlined" label="f(x,y)="></TextField>
-        </div>
-        <div className="flex flex-row p-2">
+          <Typography variant="h5" color="black">
+            f(x,y)=
+          </Typography>
           <TextField
-            variant="outlined"
+            variant="filled"
+            label="f(x,y)="
+            className="w-64"
+            onChange={(e) => ChangeEq(e.target.value)}
+          ></TextField>
+        </div>
+        <div className="flex flex-row p-2 gap-2 justify-center">
+          <TextField
+            variant="filled"
             label="X"
-            type="number"
+            type="text"
+            className=""
             InputLabelProps={{
               shrink: true,
+              color: "primary",
             }}
+            inputProps={{
+              color: "white",
+            }}
+            onChange={(e) => ChangeTX(e.target.value)}
           ></TextField>
           <TextField
-            variant="outlined"
+            variant="filled"
             label="Y"
-            type="number"
+            type="text"
             InputLabelProps={{
               shrink: true,
             }}
+            onChange={(e) => ChangeTY(e.target.value)}
           ></TextField>
         </div>
+        <div className="flex flex-col justify-center gap-2 items-center">
+          <ButtonGroup>
+            <Button
+              onClick={() => {
+                () => Prepravi(1);
+              }}
+            >
+              Riješi
+            </Button>
+            <Button>Obriši</Button>
+          </ButtonGroup>
+          <div className="flex justify-between w-full gap-6">
+            <Link href="/">
+              <Button>Početna Stranica</Button>
+            </Link>
+            <Link href="/ispit">
+              <Button>Ispitni Zadaci</Button>
+            </Link>
+          </div>
+        </div>
+        {rijeseno ? (
+          <Postupak tx={tX} ty={tY} eq={eq}></Postupak>
+        ) : (
+          <Accordion disabled></Accordion>
+        )}
       </Card>
     </>
   );
 }
 
-export default page;
+export default Page;
